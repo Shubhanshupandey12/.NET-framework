e001041d414b8167494a8eb208e281ac3fea1a35
Ritik Kaushik <107542669+Ritik74717@users.noreply.github.com>
2023-08-07 09:40:25 +0530

    Create InputOutput.cs

---
+++ InputOutput.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Enter name");
+            name=Console.ReadLine();
+            Console.WriteLine("name : {0}", name);
+            Console.ReadLine();
+        }
+    }
+}
